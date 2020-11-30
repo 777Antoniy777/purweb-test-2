e4d3a7a1583236fe762bd8bc4242481aaa75e295
777Antoniy777 <tat7106@yandex.ru>
2020-11-30 19:33:41 +0600

    create basic markdown

--- src/js/menu.js
+++
@@ -1,8 +0,0 @@
-const body = document.querySelector('body');
-const menuButton = body.querySelector('.nav__menu-button');
-
-const handleButtonClick = () => {
-  body.classList.toggle('menu-open');
-};
-
-menuButton.addEventListener('click', handleButtonClick);
